fd687602f26ee2a47d93028a58f35c797ee7e125
BOONKIT715 <chang.boon.kit@student.mmu.edu.my>
2026-05-09 21:57:54 +0800

    new update

--- data/accessories.php
+++ data/accessories.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    ['name' => 'DRY SUPER GRAP (60 WRAPS)',       'series' => 'Overgrips',          'price' => 'RM 89',    'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/i/n/int_ac149-60_011-5.png?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
+    ['name' => 'Sport Towel',       'series' => 'Towel',          'price' => 'RM 39',    'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/i/n/int_ac1111yx_171-1.png?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
     ['name' => 'DRY SUPER GRAP(3 WRAPS)',        'series' => 'Overgrips',             'price' => 'RM 18',    'tag' => 'HOT',  'img' => 'https://www.yonex.com/media/catalog/product/i/n/int_ac149-3_011-5.png?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
     ['name' => 'WRISTBAND',       'series' => 'WRISTBAND',            'price' => 'RM 39',    'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_ac498_011_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
     ['name' => 'HEADBAND',   'series' => 'HEADBAND',        'price' => 'RM 12',    'tag' => 'SALE', 'img' => 'https://www.yonex.com/media/catalog/product/a/c/ac259_007.png?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
--- data/apparel.php
+++ data/apparel.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    ['name' => 'Crew Neck Shirt',       'series' => 'Tournament',   'price' => 'RM 149',   'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_10711_602_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
-    ['name' => 'Polo Shirt',     'series' => 'Nature',      'price' => 'RM 129',   'tag' => 'NEW',  'img' => 'https://www.yonex.com/media/catalog/product/i/n/int_10759ex_684-1.png?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
-    ['name' => 'Knit Shorts ',      'series' => 'Tournament',         'price' => 'RM 99',   'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_15241_554_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
-    ['name' => 'Warm-Up Jacket',     'series' => 'Training',         'price' => 'RM 219',    'tag' => 'SALE', 'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_50189y_554_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
+    ['name' => 'T-Shirt',       'series' => '',   'price' => 'RM 120',   'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_16841_011_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
+    ['name' => 'T-Shirt',     'series' => '',      'price' => 'RM 120',   'tag' => 'NEW',  'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_16841_007_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
+    ['name' => 'Training Shorts',     'series' => 'Training',      'price' => 'RM 89',   'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_15244_011_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
+    ['name' => 'Warm-Up Jacket',      'series' => 'Tournament',         'price' => 'RM 290',   'tag' => '',     'img' => 'https://www.yonex.com/media/catalog/product/a/l/all_50191_007_1.jpg?quality=80&bg-color=248,248,248,0.75&fit=bounds&height=819&width=600&canvas=600:819'],
 ];
